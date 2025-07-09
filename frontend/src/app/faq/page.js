b3ef58600977adaa2d3aebb4c58b57ca1a02a4a2
Adrien Thevenin <contact@meilleursfilms.fr>
2025-07-09 12:31:33 +0200

    Clean: structure du projet OK, suppression dossiers inutiles

--- frontend/src/app/faq/page.js
+++
@@ -1,8 +0,0 @@
-export default function FAQPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Foire aux Questions</h1>
-      <p>Ici, vous trouverez les réponses aux questions fréquentes.</p>
-    </main>
-  );
-}
--- frontend/src/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Bienvenue sur mon site Meilleurs Films</h1>
-      <p>Ma page Next.js est désormais en ligne !</p>
-    </main>
-  );
-}
